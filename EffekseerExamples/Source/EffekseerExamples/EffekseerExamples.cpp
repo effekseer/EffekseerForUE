cbece7e20ce00321f5248cb4193f360d3aff596b
effekseer <effekseer@gmail.com>
2016-07-18 22:01:01 +0900

    initial commit

---
+++ EffekseerExamples/Source/EffekseerExamples/EffekseerExamples.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EffekseerExamples.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EffekseerExamples, "EffekseerExamples" );
