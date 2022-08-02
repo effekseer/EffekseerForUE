8a0e997f1d0e0252139117ab22f402f0ecc605ed
durswd <swda.durl@gmail.com>
2022-08-02 19:59:02 +0900

    Fix packaging script

--- CreatePackage.py
+++ CreatePackage.py
@@ -1,7 +1,7 @@
 import shutil
 import os
 
-package_name = 'EffekseerForUE4/'
+package_name = 'EffekseerForUE/'
 if os.path.exists(package_name):
     shutil.rmtree(package_name)
 os.makedirs(package_name, exist_ok=False)
@@ -10,7 +10,7 @@ shutil.copytree('Config/', package_name + 'Config/')
 shutil.copytree('Content/', package_name +'Content/')
 shutil.copytree('Plugins/', package_name +'Plugins/')
 shutil.copytree('Source/', package_name +'Source/')
-shutil.copy('EffekseerForUE4.uproject', package_name)
+shutil.copy('EffekseerForUE.uproject', package_name)
 
 shutil.copy('Help_En.html', package_name)
 shutil.copy('Help_Ja.html', package_name)
